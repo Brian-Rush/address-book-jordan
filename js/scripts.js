f254146f5cd5d63996e69e0483330a2a57005b67
Jordan Mysliwiec and Maria Del Castillo <jordan+maria@>
2017-05-08 16:43:16 -0700

    add street, city, state constructor to our address book with corresponding input fields(non functioning logic).

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ function Contact(first, last, phone, address) {
   this.firstName = first;
   this.lastName = last;
   this.phoneNumber = phone;
-  this.homeAddress = address;
+  this.addresses = [];
+}
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
@@ -11,19 +16,41 @@ Contact.prototype.fullName = function() {
 
 // user interface logic
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedPhoneNumber =
-    $("input#new-phone-number").val();
-    var inputtedHomeAddress =
-    $("input#new-home-address").val();
+    var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedHomeAddress = $("input#new-home-address").val();
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedHomeAddress);
+    $(".new-address").each(function(){
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+    //jQuery Form Submit Listener//
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
