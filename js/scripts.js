bfd40f341feaf4a3d8420bf0e8423b835986d7e5
Jordan Mysliwiec and Maria Del Castillo <jordan+maria@>
2017-05-08 11:34:42 -0700

    add form input for Phone number and Address along with necessary js logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-function Contact(first, last) {
+function Contact(first, last, phone, address) {
   this.firstName = first;
   this.lastName = last;
+  this.phoneNumber = phone;
+  this.homeAddress = address;
 }
 
 Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+  return this.firstName + " " + this.lastName ;
 }
 
 // user interface logic
@@ -14,20 +16,28 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedPhoneNumber =
+    $("input#new-phone-number").val();
+    var inputtedHomeAddress =
+    $("input#new-home-address").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedHomeAddress);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".phone-number").text(newContact.phoneNumber);
+      $(".home-address").text(newContact.homeAddress);
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+    $("input#new-home-address").val("");
 
   });
 });
